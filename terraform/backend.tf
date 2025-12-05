327e132ec234f93b8109b2557d2aec90c3d39b4a
AmKPrjectRS <alyanmo96@gmail.com>
2025-12-05 19:37:41 +0200

    update terraform files

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket  = "alianmo96-devops-terraform-state"
-    key     = "devops-todo/route53/terraform.tfstate"
-    region  = "us-west-2"
+    bucket = "bootcamp-mohammad.alian-cafa414d"
+    key    = "devops-todo/terraform.tfstate"
+    region = "us-east-2"
   }
 
   required_providers {
